b42bae033718ce94100f546874aa7e5452905dab
Johanna Sinkkonen <johanna.sinkkonen@teknomedia.se>
2021-11-18 10:10:49 +0100

    change specified node version

--- server.js
+++ server.js
@@ -3,7 +3,18 @@ const express = require("express");
 const app = express();
 app.use(express.static(__dirname + '/dist/LinkTest'));
 app.get('/*', function(req,res){
-res.sendFile(path.join(__dirname, 'dist/LinkTest', 'index.html'))
+  res.sendFile(path.join(__dirname, 'dist/LinkTest', 'index.html'))
+});
+// app.get('/hej/apple-app-site-association', function(req, res) {
+//   res.set('Content-Type', 'application/json');
+//   res.status(200)
+//   res.sendFile(path.join(__dirname + '.well-known/apple-app-site-association'))
+// })
+
+var aasa = fs.readFileSync(__dirname + '.well-known/apple-app-site-association');
+app.get('/apple-app-site-association', function(req, res, next) {
+  res.set('Content-Type', 'application/pkcs7-mime');
+  res.status(200).send(aasa);
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,14 @@ import { AppComponent } from './app.component';
 import { OpenInAppPageComponent } from './components/open-in-app.page.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: AppComponent,
-    children: [
-      {
+{
+
         path: 'applink',
         component: OpenInAppPageComponent,
       }
-    ]
 
-  }
+
+
 
 
 ];
